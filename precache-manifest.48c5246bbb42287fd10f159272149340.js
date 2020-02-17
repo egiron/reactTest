20a3a0d92a67c5f714f2c5a245ef1f79f5d4890b
Ernesto Giron <ernestogiron@Ernestos-MacBook-Air.local>
2020-02-17 14:51:32 -0500

    Updates

---
+++ precache-manifest.48c5246bbb42287fd10f159272149340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ee4f92e873c1f9f05f5c2ecd09fe5a",
+    "url": "/reactTest/index.html"
+  },
+  {
+    "revision": "8a63b9ca211b0cc35737",
+    "url": "/reactTest/static/css/main.08011531.chunk.css"
+  },
+  {
+    "revision": "b240fbbebc18886036b2",
+    "url": "/reactTest/static/js/2.e664d25c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactTest/static/js/2.e664d25c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a63b9ca211b0cc35737",
+    "url": "/reactTest/static/js/main.ad5949bf.chunk.js"
+  },
+  {
+    "revision": "d5c58a7ebf838d85fa23",
+    "url": "/reactTest/static/js/runtime-main.0b0547ae.js"
+  }
+]);
